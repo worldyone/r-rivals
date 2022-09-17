96f6bc6f7d7b5030f2be940d371cbb860f5d3ee5
worldyone <amanohikari142857@gmail.com>
2022-09-17 12:39:22 +0900

    feat: マッチングシステムの実装

--- Assets/Scripts/Onlines/OnlineMenuManager.cs
+++ Assets/Scripts/Onlines/OnlineMenuManager.cs
@@ -1,10 +1,14 @@
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OnlineMenuManager : MonoBehaviourPunCallbacks
 {
-    void Start()
+    bool isInRoom;
+    bool isMatching;
+
+    public void OnMatchingButton()
     {
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
         PhotonNetwork.ConnectUsingSettings();
@@ -13,15 +17,36 @@ public class OnlineMenuManager : MonoBehaviourPunCallbacks
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
     public override void OnConnectedToMaster()
     {
-        // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
-        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
+        PhotonNetwork.JoinRandomRoom();
     }
 
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
     public override void OnJoinedRoom()
     {
+        isInRoom = true;
+
         // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
         var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
         PhotonNetwork.Instantiate("heart", position, Quaternion.identity);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 2 }, TypedLobby.Default);
+    }
+
+    // 部屋が2人ならシーンを変える
+    void Update()
+    {
+        if (isMatching) return;
+
+        if (isInRoom)
+        {
+            if (PhotonNetwork.CurrentRoom.MaxPlayers == PhotonNetwork.CurrentRoom.PlayerCount)
+            {
+                isMatching = true;
+                SceneManager.LoadScene("Game");
+            }
+        }
     }
 }
